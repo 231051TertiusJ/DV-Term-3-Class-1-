737a4c083213341dc166a6c09b2a3f8f3d1c93a9
231051TertiusJ <125471734+231051TertiusJ@users.noreply.github.com>
2023-07-19 18:43:55 +0200

    save

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// When the doc loads
+$(document).ready(function(){
+
+    console.log("Hello")
+    //when doc loads make hero image slide up
+    $(".hero-image").animate({top: '-=100px'});
+
+}); 
